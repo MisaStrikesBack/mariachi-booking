0503c34a780c64dfad93d3d7d4c9aaf96f2d797e
misastrikesback <misaram89@gmail.com>
2018-11-17 13:22:04 -0600

    add: adding models and countries fixtures

---
+++ api/models/__init__.py
@@ -0,0 +1,4 @@
+from .locations import Country, City, Address
+from .users import Group, UserProfile
+from .hotels import Hotel, Room, Bed, BedType, Amenity, AmenityType
+from .services import Reservation, Comment
